a520e9d94ebd9a23976c518aa529b2abc82eb012
Nico Oosterwijk <nico@digitalinfo.nl>
2022-06-02 15:22:08 +0200

    moved required_providers block to terraform block

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 2.7"
+    }
+  }
+}
+
 data "terraform_remote_state" "network" {
   backend = "remote"
 
@@ -12,11 +20,6 @@ data "terraform_remote_state" "network" {
 provider "aws" {
   #version = "~> 2.7"
   region  = data.terraform_remote_state.network.outputs.aws_region
-}
-
-required_providers "aws" {
-  version = "~> 2.7"
-  #region  = data.terraform_remote_state.network.outputs.aws_region
 }
 
 data "aws_ami" "amazon_linux" {
